5f43901bc7ef4f801c4d1b8ed79eb7e5ab84dc00
Takashi <takashi@nagamotu>
2023-04-22 05:08:51 +0000

    new update

--- inc/enqueue.php
+++ inc/enqueue.php
@@ -4,15 +4,18 @@
  * @package sunset-theme
 */
 
-
+// Register and Enqueue Scripts
 function sunset_load_admin_scripts( $hook )
 {
     if ($hook !== 'toplevel_page_mranidev_sunset')
     {
         return;
     }
-    wp_register_style( 'sunset_admin', get_template_directory_uri(  ) . "/css/sunset.admin.css",array(), '1.0.0', 'all' );
-    wp_enqueue_style( 'sunset_admin' );
+    wp_register_style( 'sunset-admin', get_template_directory_uri(  ) . "/css/sunset.admin.css",array(), '1.0.0', 'all' );
+    wp_enqueue_style( 'sunset-admin' );
+
+    wp_register_script( 'sunset-admin-script', get_template_directory_uri(  ) . "/js/sunset.admin.js",array('jquery'), '1.0.0', 'all' );
+    wp_enqueue_script( 'sunset-admin-script' );
 }
 
 
